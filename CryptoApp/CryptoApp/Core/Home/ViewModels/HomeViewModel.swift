75d373cc062bc2577e7ec80b9956ba9752a875af
Ismoiljon Xolmatov <ismoiljonxolmatov637@gmail.com>
2024-03-19 11:35:50 +0500

    Download and display live market data from API

--- CryptoApp/CryptoApp/Core/Home/ViewModels/HomeViewModel.swift
+++ CryptoApp/CryptoApp/Core/Home/ViewModels/HomeViewModel.swift
@@ -9,38 +9,49 @@ import Foundation
 import Combine
 
 class HomeViewModel: ObservableObject {
-    @Published var statistics: [StatisticModel] =  [
-        StatisticModel(title: "Market Cup", value: "$12.5Bn", persentageChange: 12.65),
-        StatisticModel(title: "Total Volume", value: "$1.43Tr"),
-        StatisticModel(title: "Weekly revenue", value: "$439.6k", persentageChange: -11.98),
-        StatisticModel(title: "Mounth", value: "$2.43Tr")
-    ]
+    
+    @Published var statistics: [StatisticModel] =  []
+    
     @Published var allCoins: [CoinModel] = []
     @Published var portfolioCoins: [CoinModel] = []
     @Published var searchText: String = ""
-    private let dataService = CoinDataService()
+    
+    private let coinDataService = CoinDataService()
+    private let marketDataService = MarketDataService()
     private var cancellables = Set<AnyCancellable>()
     init() {
         addSubscribers()
     }
     
     func addSubscribers() {
-        dataService.$allCoins
+        
+        //updates all coins
+        coinDataService.$allCoins
             .sink { [weak self] returnedCoins in
                 self?.allCoins = returnedCoins
             }
             .store(in: &cancellables)
         
+        //updates based on search bar's search
         $searchText
-            .combineLatest(dataService.$allCoins)
+            .combineLatest(coinDataService.$allCoins)
             .map(filterCoins)
             .sink { [weak self] returnedCoind in
                 self?.allCoins = returnedCoind
             }
             .store(in: &cancellables)
+        
+        //updates market data
+        marketDataService.$marketDataModel
+            .map(transferMarketDataToStatModel)
+            .sink(receiveValue: { [weak self] statistics in
+                self?.statistics = statistics
+            })
+            .store(in: &cancellables)
+        
     }
     
-    func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
+    private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
         guard !text.isEmpty else { return coins }
         let lowercasedText = text.lowercased()
         return coins.filter { coin in
@@ -50,5 +61,14 @@ class HomeViewModel: ObservableObject {
         }
     }
     
-    
+    private func transferMarketDataToStatModel(marketData: MarketDataModel?) -> [StatisticModel] {
+        var stats: [StatisticModel] = []
+        guard let data = marketData else { return stats }
+        let marketCap = StatisticModel(title: "Market Cap", value: data.marketCap, persentageChange: data.marketCapPercentageChange24HUsd)
+        let volume = StatisticModel(title: "Total Volume", value: data.volume)
+        let btcDominance = StatisticModel(title: "BTC Dominance", value: data.btcDominance)
+        let portfolio = StatisticModel(title: "Portfoio Value", value: "$0.000", persentageChange: 0.000)
+        stats.append(contentsOf: [marketCap, volume, btcDominance, portfolio])
+        return stats
+    }
 }
---
+++ CryptoApp/CryptoApp/Services/MarketDataService.swift
@@ -0,0 +1,29 @@
+//
+//  MarketDataService.swift
+//  CryptoApp
+//
+//  Created by Ismoiljon Xolmatov on 19.03.2024.
+//
+
+import SwiftUI
+import Combine
+
+class MarketDataService {
+    @Published var marketDataModel: MarketDataModel? = nil
+    var marketDataSubscription: AnyCancellable?
+    
+    init () {
+        getData()
+    }
+    
+    private func getData() {
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/global") else {return}
+        marketDataSubscription = NetworkingManager.download(url: url)
+            .decode(type: GlobalData.self, decoder: JSONDecoder())
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] returnedData in
+                self?.marketDataModel = returnedData.data
+                self?.marketDataSubscription?.cancel()
+            })
+
+    }
+}
